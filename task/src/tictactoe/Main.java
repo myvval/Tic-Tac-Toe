c15ee98e60edd7ac51fe2124c61a86f6033425c8
Jakub Suchánsky <myval99@seznam.cz>
2026-04-27 16:07:08 +0200

    modified full gameState, now it determines correctly gameState

--- task/src/tictactoe/Main.java
+++ task/src/tictactoe/Main.java
@@ -6,18 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // write your code here
-        //String parts = scanner.nextLine();
-        String parts = "OOOXXXXXX";
+        String parts = scanner.nextLine();
         displayBoard(parts);
-        gameState(parts);
+        System.out.println(gameState(parts));
     }
+
     public static void displayBoard(String parts) {
-        for (int i=0;i<=3;i++) {
+        for (int i = 0; i <= 3; i++) {
             if (i == 3 || i == 0) {
                 System.out.println("---------");
-                if (i==3) continue;
+                if (i == 3) continue;
             }
-            System.out.printf("| %c %c %c |\n",parts.charAt(i*3),parts.charAt((i*3)+1),parts.charAt((i*3)+2));
+            System.out.printf("| %c %c %c |\n", parts.charAt(i * 3), parts.charAt((i * 3) + 1), parts.charAt((i * 3) + 2));
         }
     }
 
@@ -26,39 +26,42 @@ public class Main {
                 {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
                 {0, 4, 8}, {2, 4, 6}};
 
-        checkIfSomeoneWins:
+        int countX = 0, countO = 0;
+        for (int i = 0; i < parts.length(); i++) {
+            if (parts.charAt(i) == 'X') {
+                countX++;
+            } else if (parts.charAt(i) == 'O') {
+                countO++;
+            }
+        }
+        if ((countX-countO)>= 2 || (countO-countX)>=2) {
+            return "Impossible";
+        }
+
+        boolean xWins = false;
+        boolean oWins = false;
+
+
         for (int i = 0; i < winningState.length; i++) {
             char first = parts.charAt(winningState[i][0]);
             char second = parts.charAt(winningState[i][1]);
             char third = parts.charAt(winningState[i][2]);
 
-            if ((first == 'X' || first == 'O') && first == second && second == third) {
-                System.out.println(first + " wins");
+            if (first == second && second == third) {
+                switch (first) {
+                    case 'X' -> xWins = true;
+                    case 'O' -> oWins = true;
+                }
             }
-
-
-            int counter = 0;
-            char symbol = 'a';
-        /*for (int i = 0 ; i<=8;i++) {
-            symbol = parts.charAt(i);
-            if symbol
         }
+        if (xWins && oWins) {return "Impossible";}
+        else if (xWins) {return "X wins";}
+        else if (oWins) {return "O wins";}
 
-        if(parts.contains("_")) {
+        if (parts.contains("_")) {
             return "Game not finished";
-        }
-        /*else if (
-        ){
-
-        }
-        else if (!parts.contains("_")) {
+        } else {
             return "Draw";
-        }*/
-            return "";
-
-
         }
-        return "";
     }
-
 }
