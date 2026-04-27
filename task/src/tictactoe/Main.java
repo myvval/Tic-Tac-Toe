2145e68ca060987a88432abd4119cf01764d2aed
Jakub Suchánsky <myval99@seznam.cz>
2026-04-27 12:10:42 +0200

    added winning statement

--- task/src/tictactoe/Main.java
+++ task/src/tictactoe/Main.java
@@ -1,5 +1,6 @@
 package tictactoe;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
@@ -22,7 +23,32 @@ public class Main {
     }
 
     public static String gameState(String parts) {
+        int[][] winningState = {{0,1,2}, {3,4,5}, {6,7,8},
+                                {0,3,6}, {1,4,7}, {2,5,8},
+                                         {0,4,8}, {2,4,6}};
+
+
+        int counter = 0;
+        char symbol = 'a';
+        /*for (int i = 0 ; i<=8;i++) {
+            symbol = parts.charAt(i);
+            if symbol
+        }
+
+        if(parts.contains("_")) {
+            return "Game not finished";
+        }
+        /*else if (
+        ){
+
+        }
+        else if (!parts.contains("_")) {
+            return "Draw";
+        }*/
         return "";
+
+
+
     }
 
 
