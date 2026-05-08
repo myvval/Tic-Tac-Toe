559bd11470e801d891cf8dfbfc32b99de634092a
Jakub Suchánsky <myval99@seznam.cz>
2026-05-08 13:02:10 +0200

    refactored user input(coordinates) ifs in main

--- task/src/tictactoe/Main.java
+++ task/src/tictactoe/Main.java
@@ -15,39 +15,37 @@ public class Main {
 
         while (true) {
             System.out.println("Enter the coordinates for your move (e.g., 1 1):");
-            Integer row = null;
-            Integer col = null;
-            int index = -1;
-            //assign value to index, need to check if index got changed, moved check from bottom to this stacked if statement
-            if (scanner.hasNextInt()) {
-                row = scanner.nextInt();
-                if (scanner.hasNextInt()) {
-                    col = scanner.nextInt();
-                    if (col <= 3 && col >= 1 && row <= 3 && row >=1 ) {
-                        index = (row - 1) * 3 + (col - 1);
 
-                        if (parts.charAt(index) == '_') {
-
-                            parts = parts.substring(0,index) + 'X' + parts.substring(index+1);
-                            displayBoard.printBoard(parts);
-                            break;
-                        } else {
-                            System.out.println("This cell is occupied! Choose another one!");
-                        }
-
-                    } else {
-                        System.out.println("Coordinates should be from 1 to 3!");
-                    }
-                } else {
-                    System.out.println("You should enter numbers!");
-                }
-            } else {
+            if (!scanner.hasNextInt()) {
                 System.out.println("You should enter numbers!");
+                scanner.nextLine();
+                continue;
             }
-            row = null;
-            col = null;
-        }
+            int row = scanner.nextInt();
 
+            if (!scanner.hasNextInt()) {
+                System.out.println("You should enter numbers!");
+                scanner.nextLine();
+                continue;
+            }
+            int col = scanner.nextInt();
+
+            if (!(col <= 3 && col >= 1 && row <= 3 && row >= 1)) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+            int index = (row - 1) * 3 + (col - 1);
+
+            if (parts.charAt(index) == '_') {
+
+                parts = parts.substring(0, index) + 'X' + parts.substring(index + 1);
+                displayBoard.printBoard(parts);
+                break;
+            } else {
+                System.out.println("This cell is occupied! Choose another one!");
+
+            }
+        }
 
     }
     public static String gameState(String parts) {
