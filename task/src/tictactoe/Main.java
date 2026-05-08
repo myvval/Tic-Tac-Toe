c72fb1ebb89a8502d99705318b64e6ee190520a0
Jakub Suchánsky <myval99@seznam.cz>
2026-05-08 12:56:35 +0200

    user can now play 1 round as X

--- task/src/tictactoe/Main.java
+++ task/src/tictactoe/Main.java
@@ -1,27 +1,52 @@
 package tictactoe;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // write your code here
         String parts = scanner.nextLine();
+        //print 3x3 gird
         displayBoard.printBoard(parts);
-        System.out.println(gameState(parts));
-        System.out.println("Enter the coordinates for your move (e.g., 1 1):");
-        Integer row = null;
-        Integer col = null;
-        int index = -1;
-        //assign value to index, need to check if index got changed, moved check from bottom to this stacked if statement
-        if (scanner.hasNextInt()) {
-            row = scanner.nextInt();
-            if (scanner.hasNextInt()) {
-                col = scanner.nextInt();
-                index = (row-1)*3+(col-1);
-                if (!(index >=0 & index<=9)) {System.out.println("Invalid index.");}
-            } else {System.out.println("Invalid index.");}
-        } else {System.out.println("Invalid index.");}
 
+        //print gameState
+        System.out.println(gameState(parts));
+
+        while (true) {
+            System.out.println("Enter the coordinates for your move (e.g., 1 1):");
+            Integer row = null;
+            Integer col = null;
+            int index = -1;
+            //assign value to index, need to check if index got changed, moved check from bottom to this stacked if statement
+            if (scanner.hasNextInt()) {
+                row = scanner.nextInt();
+                if (scanner.hasNextInt()) {
+                    col = scanner.nextInt();
+                    if (col <= 3 && col >= 1 && row <= 3 && row >=1 ) {
+                        index = (row - 1) * 3 + (col - 1);
+
+                        if (parts.charAt(index) == '_') {
+
+                            parts = parts.substring(0,index) + 'X' + parts.substring(index+1);
+                            displayBoard.printBoard(parts);
+                            break;
+                        } else {
+                            System.out.println("This cell is occupied! Choose another one!");
+                        }
+
+                    } else {
+                        System.out.println("Coordinates should be from 1 to 3!");
+                    }
+                } else {
+                    System.out.println("You should enter numbers!");
+                }
+            } else {
+                System.out.println("You should enter numbers!");
+            }
+            row = null;
+            col = null;
+        }
 
 
     }
